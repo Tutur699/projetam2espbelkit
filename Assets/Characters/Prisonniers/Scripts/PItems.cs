64a449f28eacfd1da4e769a4286ea087c52c86d2
kyuubantai <djessykita2@outlook.com>
2025-09-26 16:31:45 +0200

    Modif WPManager + PItems

--- Assets/Characters/Prisonniers/Scripts/PItems.cs
+++ Assets/Characters/Prisonniers/Scripts/PItems.cs
@@ -1,11 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
-public class PItems
+public class PItems : MonoBehaviour
 {
-    public GameObject itemObject;
+    public GameObject itemObject; //The actual item object in the scene
+    public bool singleUse = false;
+    public float useRate = 0.1f;
+    public float weaponDamage = 15; //How much damage should this weapon deal
+
+    [HideInInspector]
     public WPManager manager;
     public void ActivateWeapon(bool activate)
     {
         itemObject.SetActive(activate);
+    }
+
+    void Start()
+    {
+        if (manager == null)
+        {
+            manager = FindFirstObjectByType<WPManager>();
+        }
+
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && singleUse)
+        {
+            Use();
+        }
+        if (Input.GetMouseButton(0) && !singleUse)
+        {
+            Use();
+        }
+    }
+    void Use()
+    {
+        //Implement the use logic here
+        Debug.Log("Using item: " + gameObject.name);
     }
 }
--- Assets/Characters/Prisonniers/Scripts/WPManager.cs
+++ Assets/Characters/Prisonniers/Scripts/WPManager.cs
@@ -2,87 +2,88 @@ using UnityEngine;
 
 public class WPManager : MonoBehaviour
 {
-    public Camera playerCamera;
-    public PItems[] items;
-    private int selectedItemIndex;
+    public const int MAXITEMS = 3;
+     public Camera playerCamera;
+     public PItems[] items = new PItems[MAXITEMS];
+     private int selectedItemIndex;
 
-    [HideInInspector]
-    public PItems selectedItems;
+     [HideInInspector]
+     public PItems selectedItems;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //At the start we enable the primary weapon and disable the rest
-        for (selectedItemIndex = 0; selectedItemIndex < items.Length; selectedItemIndex++)
-        {
-            if (selectedItemIndex == 0)
-                items[selectedItemIndex].ActivateWeapon(true);
-            else
-                items[selectedItemIndex].ActivateWeapon(false);
-            items[selectedItemIndex].manager = this;
-        }
-    }
+     // Start is called before the first frame update
+     void Start()
+     {
+         //At the start we enable the primary weapon and disable the rest
+         for (selectedItemIndex = 0; selectedItemIndex < items.Length; selectedItemIndex++)
+         {
+             if (selectedItemIndex == 0)
+                 items[selectedItemIndex].ActivateWeapon(true);
+             else
+                 items[selectedItemIndex].ActivateWeapon(false);
+             items[selectedItemIndex].manager = this;
+         }
+     }
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        //Select primary weapon when pressing 1
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-         items[0].ActivateWeapon(true);
-            selectedItems = items[0];
-            for (selectedItemIndex = 1; selectedItemIndex < items.Length; selectedItemIndex++)
-            {
-             items[selectedItemIndex].ActivateWeapon(false);
-            }
-        }
+     /*// Update is called once per frame
+     /*void Update()
+     {
+         //Select primary weapon when pressing 1
+         if (Input.GetKeyDown(KeyCode.Alpha1))
+         {
+          items[0].ActivateWeapon(true);
+             selectedItems = items[0];
+             for (selectedItemIndex = 1; selectedItemIndex < items.Length; selectedItemIndex++)
+             {
+              items[selectedItemIndex].ActivateWeapon(false);
+             }
+         }
 
-        //Select secondary weapon when pressing 2
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-         items[1].ActivateWeapon(true);
-            selectedItems = items[1];
-         items[0].ActivateWeapon(false);
-            for (selectedItemIndex = 2; selectedItemIndex < items.Length; selectedItemIndex++)
-            {
-             items[selectedItemIndex].ActivateWeapon(false);
-            }
-        }
-        //Select third weapon when pressing 3
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-         items[2].ActivateWeapon(true);
-            selectedItems = items[2];
-         items[0].ActivateWeapon(false);
-         items[1].ActivateWeapon(false);
-            for (selectedItemIndex = 3; selectedItemIndex < items.Length; selectedItemIndex++)
-            {
-             items[selectedItemIndex].ActivateWeapon(false);
-            }
-        }
-        //Select fourth weapon when pressing 4
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-         items[3].ActivateWeapon(true);
-            selectedItems = items[3];
-         items[0].ActivateWeapon(false);
-         items[1].ActivateWeapon(false);
-         items[2].ActivateWeapon(false);
-            for (selectedItemIndex = 4; selectedItemIndex < items.Length; selectedItemIndex++)
-            {
-             items[selectedItemIndex].ActivateWeapon(false);
-            }
-        }
-        //Select last weapon when pressing 5
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-         items[4].ActivateWeapon(true);
-            selectedItems = items[4];
-            for (selectedItemIndex = 0; selectedItemIndex < items.Length - 1; selectedItemIndex++)
-            {
-             items[selectedItemIndex].ActivateWeapon(false);
-            }
-        }
-    }
-}*/
+         //Select secondary weapon when pressing 2
+         if (Input.GetKeyDown(KeyCode.Alpha2))
+         {
+          items[1].ActivateWeapon(true);
+             selectedItems = items[1];
+          items[0].ActivateWeapon(false);
+             for (selectedItemIndex = 2; selectedItemIndex < items.Length; selectedItemIndex++)
+             {
+              items[selectedItemIndex].ActivateWeapon(false);
+             }
+         }
+         //Select third weapon when pressing 3
+         if (Input.GetKeyDown(KeyCode.Alpha3))
+         {
+          items[2].ActivateWeapon(true);
+             selectedItems = items[2];
+          items[0].ActivateWeapon(false);
+          items[1].ActivateWeapon(false);
+             for (selectedItemIndex = 3; selectedItemIndex < items.Length; selectedItemIndex++)
+             {
+              items[selectedItemIndex].ActivateWeapon(false);
+             }
+         }
+         //Select fourth weapon when pressing 4
+         if (Input.GetKeyDown(KeyCode.Alpha4))
+         {
+          items[3].ActivateWeapon(true);
+             selectedItems = items[3];
+          items[0].ActivateWeapon(false);
+          items[1].ActivateWeapon(false);
+          items[2].ActivateWeapon(false);
+             for (selectedItemIndex = 4; selectedItemIndex < items.Length; selectedItemIndex++)
+             {
+              items[selectedItemIndex].ActivateWeapon(false);
+             }
+         }
+         //Select last weapon when pressing 5
+         if (Input.GetKeyDown(KeyCode.Alpha5))
+         {
+          items[4].ActivateWeapon(true);
+             selectedItems = items[4];
+             for (selectedItemIndex = 0; selectedItemIndex < items.Length - 1; selectedItemIndex++)
+             {
+              items[selectedItemIndex].ActivateWeapon(false);
+             }
+         }
+     }
+ }*/
 }
