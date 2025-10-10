1bd5580cd589f3aac45986956e0f309661913fc9
kyuubantai <djessykita2@outlook.com>
2025-10-10 17:02:56 +0200

    Ajout variables pour affichage de l'inventaire

--- Assets/Characters/Prisonniers/Scripts/WPManager.cs
+++ Assets/Characters/Prisonniers/Scripts/WPManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class WPManager : MonoBehaviour
 {
@@ -7,6 +8,11 @@ public class WPManager : MonoBehaviour
     public Camera playerCamera;
     public List<PItems> items = new List<PItems>(MAXITEMS);
     public int selectedItemIndex;
+
+    public Image[] itemIcons = new Image[5]; //Array to hold the UI icons for the items
+    public Image[] itemBackgrounds = new Image[5]; //Array to hold the UI backgrounds for the item icons
+    public Sprite defaultIcon; //Default icon to use when no item is equipped
+
 
     [HideInInspector]
     public PItems selectedItems;
@@ -25,6 +31,25 @@ public class WPManager : MonoBehaviour
     {
         //At the start we enable the primary weapon and disable the rest
         SelectItems(0);
+    }
+
+    public void Update() //Mise à jour de l'interface utilisateur
+    {
+        /*for (int i = 0; i < 5; i++)
+        {
+            if (i < items.Count && items[i] != null)
+            {
+                itemIcons[i].sprite = items[i].icon;
+                itemIcons[i].enabled = true;
+                itemBackgrounds[i].enabled = true;
+            }
+            else
+            {
+                itemIcons[i].sprite = defaultIcon;
+                itemIcons[i].enabled = false;
+                itemBackgrounds[i].enabled = false;
+            }
+        }*/
     }
     public void SelectItems(int index) //Méthode pour sélectionner une arme dans l'inventaire
     {
